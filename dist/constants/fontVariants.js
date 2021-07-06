2ceedd95cee102b494a699c94206bcd55ca6cdc4
yandri <>
2021-07-06 14:56:38 +0700

    tmp

--- dist/constants/fontVariants.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;var _default={STYLE:{NORMAL:"normal",ITALIC:"italic",OBLIQUE:"oblique"},TRANSFORM:{UPPERCASE:"uppercase",NORMAL:"normal",LOWERCASE:"lowercase"},WEIGHT:{LIGHT:"light",REGULAR:"regular",SEMI_BOLD:"semiBold",BOLD:"bold",EXTRA_BOLD:"extraBold"}};exports["default"]=_default;
--- dist/constants/statusTypes.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;var _default={INIT:"init",LOADING:"loading",SUCCESS:"success",ERROR:"error"};exports["default"]=_default;
